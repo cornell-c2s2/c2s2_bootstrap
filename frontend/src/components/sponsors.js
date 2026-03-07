4f7fad3acdd6053f4214963131c00873efb92ff5
adiprathapa <aprathapa01@gmail.com>
2026-03-07 15:30:34 -0500

    updated sponsors

--- frontend/src/components/sponsors.js
+++ frontend/src/components/sponsors.js
@@ -1,5 +1,5 @@
 import React from "react";
-import cadence from "../assets/img/sponsors/cadence.png";
+import nordtech from "../assets/img/sponsors/nordtech.png";
 import amd from "../assets/img/sponsors/amd.png";
 
 function Sponsors() {
@@ -27,28 +27,7 @@ function Sponsors() {
       <section id="about" className="sponsors">
         <div className="container" data-aos="fade-up">
           <div className="section-title underline">
-            <h1>Primary Sponsor</h1>
-          </div>
-
-          <div data-aos="zoom-in" data-aos-delay="100">
-            <p>
-              C2S2's primary sponsorship comes from the{" "}
-              <strong>Shen Fund for Social Impact</strong>. The fund is named
-              after Cornell Engineering alumnus David T. Shen (Class of '89),
-              managing director of the private equity group Olympus Capital
-              Holdings Asia. The fund is targeted to bring together new student
-              teams under a faculty member's mentorship to address significant
-              social challenges in society today through novel and/or advanced
-              engineering solutions. C2S2 addresses this not only through our
-              partnership with the Lab of Ornithology, but through our
-              contributions back to the open-source chip design ecosystem. The
-              fund generously supports C2S2 over a period of 3 years, and has
-              been the enabler for C2S2 to start as a project team.
-            </p>
-          </div>
-
-          <div className="section-title underline">
-            <h1>Silicon Sponsors</h1>
+            <h1>Current Sponsors</h1>
           </div>
 
           <div className="row justify-content-center align-items-center" style={{ padding: "30px 0" }}>
@@ -58,9 +37,9 @@ function Sponsors() {
               data-aos-delay="100"
             >
               <div className="sponsor">
-                <a target="_blank" href="https://cadence.com">
+                <a target="_blank" href="https://nordtech.com">
                   <div className="c2s2-alumni-logo-container" style={{ width: "260px", height: "170px" }}>
-                    <img src={cadence} alt="Cadence Design Systems" className="c2s2-alumni-company-logo" />
+                    <img src={nordtech} alt="Nordtech" className="c2s2-alumni-company-logo" />
                   </div>
                 </a>
               </div>
