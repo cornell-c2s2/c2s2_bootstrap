14bb82553d9d1fea8a2c126b086fc9f239a88654
Joseph <josephwan.nc@gmail.com>
2026-08-17 11:00:04 -0400

    fall 26 applications

--- src/components/apply.js
+++ src/components/apply.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Apply = () => {
-  const applicationFormUrl =
-    "https://docs.google.com/forms/d/e/1FAIpQLSehuTjELRT6yKGaCuY0TXnjhR4eqpTaZeajI8UMLWceYO_0QA/viewform?usp=publish-editor";
+  const upperclassmenFormUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSfOtUdHIxSDMXAi9Sor3kFOB2UwRepmBt7YxTGUKBilWGz3nQ/viewform?usp=dialog";
+  const firstYearTransferFormUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLScJM43tTYX9PDSqFV5Y_KXmL_Jm8z4jTIwN4YJHQwDHLbMGKw/viewform?usp=dialog";
 
   return (
     <>
       <main id="main">
         <section id="apply" className="breadcrumbs">
           <div className="container">
-            <div className="section-title">
-              
-            </div>
+            <div className="section-title"></div>
           </div>
         </section>
 
@@ -22,8 +22,14 @@ const Apply = () => {
             </div>
 
             <div className="row justify-content-center">
+              <div className="col-12 text-center mb-4">
+                <h4 className="title">Applications are open!</h4>
+              </div>
+            </div>
+
+            <div className="row">
               <div
-                className="col-md-6 col-lg-6 d-block align-items-center"
+                className="col-md-6 col-lg-6 col-sm-12 d-block align-items-center"
                 data-aos="zoom-in"
                 data-aos-delay="100"
               >
@@ -31,15 +37,36 @@ const Apply = () => {
                   <div className="icon">
                     <i className="bx bx-file"></i>
                   </div>
-                  <h4 className="title">Applications are not currently open!</h4>
-                  <p className="description">
-                    
-                  </p>
-                  <p className="description">
-                    <a href={applicationFormUrl} target="_blank" rel="noreferrer">
-                      Open application form
-                    </a>
-                  </p>
+                  <h4 className="title">Upperclassmen Application</h4>
+                  <a
+                    href={upperclassmenFormUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="application-btn"
+                  >
+                    Open Application Form
+                  </a>
+                </div>
+              </div>
+
+              <div
+                className="col-md-6 col-lg-6 col-sm-12 d-block align-items-center"
+                data-aos="zoom-in"
+                data-aos-delay="200"
+              >
+                <div className="icon-box">
+                  <div className="icon">
+                    <i className="bx bx-file"></i>
+                  </div>
+                  <h4 className="title">First Year & Transfer Application</h4>
+                  <a
+                    href={firstYearTransferFormUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="application-btn"
+                  >
+                    Open Application Form
+                  </a>
                 </div>
               </div>
             </div>
